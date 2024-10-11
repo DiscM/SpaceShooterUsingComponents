48f39a9bb3d48391ec4fb34d0ab511c56f58da73
DiscM <fusionfall.salabao@gmail.com>
2024-10-11 07:26:21 -0700

    MoveComponent/Input
    Added in basic move components and interchangeable input components.

---
+++ components/move_input_component.gd
@@ -0,0 +1,10 @@
+class_name MoveInputComponent
+extends Node
+
+##Move input component takes input from the user and sets the velocity on the Move Component
+@export var move_component: MoveComponent
+
+func _input(event: InputEvent) -> void:
+	##get_axis will return either 1 or -1 depending on ui returned, 0 is assumed if neither is pressed
+	var input_axis = Input.get_axis("ui_left", "ui_right")
+	move_component.velocity = Vector2(input_axis * 50, 0)
